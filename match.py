3c8e1c7b3ab165ae23b58672a313f551742248a8
afpo <poorkhanali@gmail.com>
2025-11-29 12:58:41 +0100

    Implemented test point matching

---
+++ match.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+# Load datasets
+
+# Store matched results
+
+# We have 4 training functions: y1, y2, y3, y4
+    
+## Compare against all 50 ideal functions (y1..y50)
+  ## skip "x"
+### Compute Sum of Squared Errors (SSE)
