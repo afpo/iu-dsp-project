d64daee8c884a3ec6f5a0f8885633959796ab9b3
afpo <poorkhanali@gmail.com>
2025-11-29 13:02:26 +0100

    Implemented test point visualization

---
+++ visualize.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import Legend, ColumnDataSource
+from bokeh.palettes import Category10
+from math import sqrt
+
+# Load data
+
+# Matches discovered earlier
+
+# Bokeh output HTML file
+
+# Create figure
+
+# ===========================================================
+# 1. Plot TRAINING FUNCTIONS (y1–y4)
+# ===========================================================
+
+# ===========================================================
+# 2. Plot MATCHED IDEAL FUNCTIONS (y13, y24, y36, y40)
+# ===========================================================
+
+# ===========================================================
+# 3. Plot CLASSIFIED TEST POINTS (scatter)
+# ===========================================================
+# Normal classified points
+
+# Outliers (None classification)
+
+# Attach legend
+
+# Show plot
+
